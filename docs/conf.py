bad67fa1133aaf3c6de4eb6b4ec185ab04f53e54
pasla <paslaframework@gmail.com>
2023-12-30 22:01:07 +0700

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -1,42 +1,41 @@
 #conf.py
 
-#information
+# Information
+project = 'pasla'
+copyright = '2023'
+author = 'Panji Satria Taqwa Putra Purnama'
 
-project='pasla'
-copyright='2023'
-author='Panji Satria Taqwa Putra Purnama'
-
-#this is configuration--------------------
-#add any Sphinx extension module
-#coming with Sphinx (named'sphinx.ext.*')
-extensions=[
-'sphinx.ext.autodoc', 
-'sphinx.ext.napoleon',
-'sphinx.ext.viewcode',
+# This is configuration--------------------
+# Add any Sphinx extension module
+# coming with Sphinx (named 'sphinx.ext.*')
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
 
-#master-------------------------------------
-master_doc='index'
+# Master-------------------------------------
+master_doc = 'index'
 
-#variable------------------------------------
-project='pasla'
-copyright='2023'
-author='Panji Satria Taqwa Putra Purnama'
+# Variable------------------------------------
+project = 'pasla'
+copyright = '2023'
+author = 'Panji Satria Taqwa Putra Purnama'
 
-#theme---------------------------------------
-html_theme='alabaster'
+# Theme---------------------------------------
+html_theme = 'alabaster'
 
-#path-theme---------------------------------
-html_static_path=['_static']
+# Path-theme---------------------------------
+html_static_path = ['_static']
 
-#rest----------------------------------------
-html-show_sourcelink = True
+# Rest----------------------------------------
+html_show_sourcelink = True
 
-#sphinx-v-----------------------------------
-#needs_sphinx='1.0'
+# Sphinx-v-----------------------------------
+# needs_sphinx='1.0'
 
-#google-docstring--------------------------
-napoleon_goole_docstring= True
+# Google-docstring--------------------------
+napoleon_google_docstring = True
 
-#doc-page---------------------------------
-viewcode_follow_imported_members= True
+# Doc-page---------------------------------
+viewcode_follow_imported_members = True
